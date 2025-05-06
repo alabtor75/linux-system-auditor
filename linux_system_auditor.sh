ad3d7d9dce8fc643c9dae892ac108b66a5e3b6c6
Soufianne Nassibi <soufianne.nassibi@gmail.com>
2025-05-06 15:02:54 +0100

    Reset: Clean base for Linux System Auditor v1.1

--- linux_system_auditor.sh
+++ linux_system_auditor.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
+# Linux System Auditor
 # Author: Soufianne Said NASSIBI
-# Version: 1.1 (with AI option)
+# Version: 1.1 (with AI option) 05/2025
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 CYAN='\033[0;36m'
